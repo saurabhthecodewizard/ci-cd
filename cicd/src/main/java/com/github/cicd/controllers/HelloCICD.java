0fac8e65f4f1e0ac1c8a2df24cb696ae7f9ada16
saurabhthecodewizard <skmahajan4998@gmail.com>
2024-01-20 10:51:15 +0530

    modified api

--- cicd/src/main/java/com/github/cicd/controllers/HelloCICD.java
+++ cicd/src/main/java/com/github/cicd/controllers/HelloCICD.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloCICD {
     @GetMapping("/")
     public String helloCicd() {
-        return "Hello CICD";
+        return "Hello CICD First Blood";
     }
 }
